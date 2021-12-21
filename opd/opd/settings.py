249fc6858735b1e8af7c1b25f8f0d446aa96777a
T168 <thitiwut.tra@gmail.com>
2021-12-21 13:57:17 +0700

    Use google font and static images

--- opd/opd/settings.py
+++ opd/opd/settings.py
@@ -115,8 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
+STATICFILES_DIRS = (
+    Path(BASE_DIR , 'assets') , 
+)
+
+# STATIC_ROOT = Path(BASE_DIR, 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
